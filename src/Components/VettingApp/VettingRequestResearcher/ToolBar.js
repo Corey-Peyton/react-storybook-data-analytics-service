ade9f68659121e1cb8d0d1e1e5a03779f4ad2436
stevensam0109 <steven.sam.0109@outlook.com>
2020-10-23 12:33:32 -0400

    Got rid of errors

--- src/Components/VettingApp/VettingRequestResearcher/ToolBar.js
+++ src/Components/VettingApp/VettingRequestResearcher/ToolBar.js
@@ -1,50 +1,58 @@
 import React from 'react';
-import {Button, Toolbar} from '@material-ui/core';
+import {useStyles} from '@material-ui/core/styles';
+import {Button, Toolbar, IconButton, ArrowBackIcon, Typography} from '@material-ui/core';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import SaveIcon from '@material-ui/icons/Save';
 import SendIcon from '@material-ui/icons/Send';
 
-function ToolBar () {
-return (
-  <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <ArrowBackIcon />
-            </IconButton>
-            <Typography variant="body2" className={classes.title}>
+const handleDialogOpen = () => {
+  setOpen(true);
+};
+
+const setOpen = React.useState(false);
+
+function ToolBar() {
+  const classes = useStyles();
+  return (
+    <Toolbar>
+      <IconButton
+        edge="start"
+        className={classes.menuButton}
+        color="inherit"
+        aria-label="menu"
+      >
+        <ArrowBackIcon />
+      </IconButton>
+      <Typography variant="body2" className={classes.title}>
               Vetting requests dashboard
-            </Typography>
-            <Button
-              color="default"
-              className={classes.headerBtn}
-              startIcon={<ExitToAppIcon />}
-              onClick={handleDialogOpen}
-            >
+      </Typography>
+      <Button
+        color="default"
+        className={classes.headerBtn}
+        startIcon={<ExitToAppIcon />}
+        onClick={handleDialogOpen}
+      >
               Withdraw
-            </Button>
-            <Button
-              variant="outlined"
-              color="primary"
-              className={classes.headerBtn}
-              startIcon={<SaveIcon />}
-            >
+      </Button>
+      <Button
+        variant="outlined"
+        color="primary"
+        className={classes.headerBtn}
+        startIcon={<SaveIcon />}
+      >
               Save
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.headerBtn}
-              startIcon={<SendIcon />}
-            >
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.headerBtn}
+        startIcon={<SendIcon />}
+      >
               Submit request
-            </Button>
-          </Toolbar>
-)
+      </Button>
+    </Toolbar>
+  );
 }
 
-export default ToolBar
+export default ToolBar;
 
--- src/Components/VettingApp/VettingRequestResearcher/ToolBarDelete.js
+++ src/Components/VettingApp/VettingRequestResearcher/ToolBarDelete.js
@@ -1,57 +1,59 @@
 import React from 'react';
-import {Button, Toolbar} from '@material-ui/core';
+import {useStyles} from '@material-ui/core/styles';
+import {Button, Toolbar, IconButton, ArrowBackIcon, Typography} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ReplayIcon from '@material-ui/icons/Replay';
 import SaveIcon from '@material-ui/icons/Save';
 import SendIcon from '@material-ui/icons/Send';
 
-function ToolBarDelete () {
-return (
-  <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <ArrowBackIcon />
-            </IconButton>
-            <Typography variant="body2" className={classes.title}>
+function ToolBarDelete() {
+  const classes = useStyles();
+  return (
+    <Toolbar>
+      <IconButton
+        edge="start"
+        className={classes.menuButton}
+        color="inherit"
+        aria-label="menu"
+      >
+        <ArrowBackIcon />
+      </IconButton>
+      <Typography variant="body2" className={classes.title}>
               Vetting requests dashboard
-            </Typography>
-            <Button
-              color="default"
-              className={classes.headerBtn}
-              startIcon={<ReplayIcon />}
-            >
-              Convert to draft 
-            </Button>
-            <Button
-              variant="outlined"
-              color="primary"
-              className={classes.headerBtn}
-              startIcon={<SaveIcon />}
-            >
+      </Typography>
+      <Button
+        color="default"
+        className={classes.headerBtn}
+        startIcon={<ReplayIcon />}
+      >
+              Convert to draft
+      </Button>
+      <Button
+        variant="outlined"
+        color="primary"
+        className={classes.headerBtn}
+        startIcon={<SaveIcon />}
+      >
               Save
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.headerBtn}
-              startIcon={<SendIcon />}
-            >
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.headerBtn}
+        startIcon={<SendIcon />}
+      >
               Submit request
-            </Button>
-            <Button              
-              color="default"
-              className={classes.headerBtn}
-              startIcon={<DeleteIcon />}
-            >
+      </Button>
+      <Button
+        color="default"
+        className={classes.headerBtn}
+        startIcon={<DeleteIcon />}
+      >
               Delete
-            </Button>
-          </Toolbar>
-)
+      </Button>
+    </Toolbar>
+  );
 }
 
-export default ToolBarDelete
+export default ToolBarDelete;
 
