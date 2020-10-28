9b1ab607ca760b9976881306058e6b8cec39a500
stevensam0109 <steven.sam.0109@outlook.com>
2020-10-28 16:55:20 -0400

    Tabpanels extracted

--- src/Components/VettingApp/Dashboard/DashboardSupport.js
+++ src/Components/VettingApp/Dashboard/DashboardSupport.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import {makeStyles} from '@material-ui/core/styles';
 import {
   AppBar,
   Typography,
-  Box,
 } from '@material-ui/core';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -13,6 +11,7 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 
 import TableContainerComponent from './TableContainerComponent';
+import TabPanel from './DashboardTabPanel';
 import Header from '../CommonComponents/Header';
 import Footer from '../CommonComponents/Footer';
 import DashboardDrawer from './DashboardDrawer';
@@ -53,7 +52,7 @@ const useStyles = makeStyles((theme) => ({
   content: {
     background: '#F7F7F7',
     padding: theme.spacing(0, 3, 3, 3),
-    paddingTop: `calc(24px + 64px)`,
+    paddingTop: `88px`,
     minHeight: `calc(100vh - 284px)`,
     flexGrow: 1,
     transition: theme.transitions.create('margin', {
@@ -72,23 +71,6 @@ const useStyles = makeStyles((theme) => ({
   button: {
     height: 36,
   },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1,
-  },
-  thead: {
-    width: '15.7%',
-  },
-  theadNarrow: {
-    width: '7%',
-  },
   tabs: {
     borderBottom: '1px solid',
     borderBottomColor: theme.palette.grey[300],
@@ -100,11 +82,6 @@ const useStyles = makeStyles((theme) => ({
       boxSizing: 'border-box',
       overflowY: 'auto',
       boxShadow: '0px 2px 4px -1px rgba(117,117,117,0.2), 0px 4px 5px 0px rgba(117,117,117,0.14), 0px 1px 10px 0px rgba(117,117,117,0.12)',
-    },
-  },
-  tableHead: {
-    '& th': {
-      padding: theme.spacing(2.5, 1),
     },
   },
 }));
@@ -123,30 +100,6 @@ const filteredRows = () => {
   return (
     rows.filter((val) => val.statusHead === tabStatus)
   );
-};
-
-// /////////// TABPANEL
-function TabPanel(props) {
-  const {children, value, index, ...other} = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`projects-tabpanel-${index}`}
-      aria-labelledby={`projects-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
 };
 
 function a11yProps(index) {
