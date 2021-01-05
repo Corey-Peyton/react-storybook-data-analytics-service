b1cc9804b74f27f2d8570ff7b4c72e869435ee89
stevensam0109 <steven.sam.0109@outlook.com>
2021-01-05 14:42:30 -0500

    remove comments

--- src/Components/VettingApp/Dashboard/DashboardPageResearcher.js
+++ src/Components/VettingApp/Dashboard/DashboardPageResearcher.js
@@ -93,9 +93,9 @@ const rows = requestListResearchers.map((el, index) =>
 const headCells = [
   {id: 'id', narrow: false, disablePadding: true, label: 'ID'},
   {id: 'status', narrow: false, disablePadding: false, label: 'Status'},
-  {id: 'researcher', narrow: false, disablePadding: false, label: 'Researcher'}, // not sorting
+  {id: 'researcher', narrow: false, disablePadding: false, label: 'Researcher'},
   {id: 'lead', narrow: false, disablePadding: false, label: 'Analyst'},
-  {id: 'created', narrow: false, disablePadding: false, label: 'Created on'}, // not sorting
+  {id: 'created', narrow: false, disablePadding: false, label: 'Created on'},
   {id: 'updated', narrow: false, disablePadding: false, label: 'Updated on'},
   {id: 'actions', narrow: true, disablePadding: false, label: 'Actions'},
 ];
