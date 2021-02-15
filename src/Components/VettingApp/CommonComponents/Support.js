60be7e5982a6bb0fa568f8b1d715c02d21acb7bc
stevensam0109 <steven.sam.0109@outlook.com>
2021-02-15 15:22:21 -0500

    Added comments field and tweaked styles

--- src/Components/VettingApp/CommonComponents/Support.js
+++ src/Components/VettingApp/CommonComponents/Support.js
@@ -11,6 +11,7 @@ import {
   Button,
   Divider,
   Typography,
+  TextField,
 } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 
@@ -23,8 +24,24 @@ const useStyles = makeStyles((theme) => ({
       top: '50%',
     },
   },
+  dialog: {
+    '& .MuiDialog-paperWidthSm': {
+      'width': 400,
+      '& .MuiTextField-root': {
+        width: '100%',
+      },
+    },
+  },
   extendedIcon: {
     margin: theme.spacing(1),
+  },
+  dialogFooter: {
+    padding: theme.spacing(3, 3),
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  footerBtns: {
+    marginLeft: [theme.spacing(2), '!important'],
   },
 }));
 
@@ -43,11 +60,17 @@ export default function FloatingSupportButton() {
     setOpen(false);
   };
 
-
   return (
     <div className={classes.root}>
-      <Fab color="primary" variant="extended" aria-label="support" size="medium" onClick={handleClickOpen}>
-        <Email className={classes.extendedIcon}/>Get support
+      <Fab
+        color="primary"
+        variant="extended"
+        aria-label="support"
+        size="medium"
+        onClick={handleClickOpen}
+      >
+        <Email className={classes.extendedIcon} />
+        Get support
       </Fab>
       <Dialog
         open={open}
@@ -55,31 +78,40 @@ export default function FloatingSupportButton() {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
         fullWidth="false"
+        className={classes.dialog}
       >
-        <DialogTitle id="alert-dialog-title">
-          Get support
-        </DialogTitle>
-        <DialogContent className="pb-0">
+        <DialogTitle id="alert-dialog-title">Get support</DialogTitle>
+        <Divider />
+        <DialogContent className="pb-0 mt-1">
           <DialogContentText id="alert-dialog-description">
-            <Typography variant="body2" color="textPrimary">Click submit and one of our support staff will reach out to help you with your enquiry.</Typography>
+            <Typography variant="body2" color="textPrimary">
+              Click submit and one of our support staff will reach out to help
+              you with your enquiry.
+            </Typography>
           </DialogContentText>
-          <Divider className={classes.divider} />
-        </DialogContent>
-        <DialogActions className={classes.dialogActions}>
-          <Button
-            onClick={handleClose}
-            color="primary"
+          <TextField
+            className="mt-1 mb-3"
+            id="comment-text"
+            label="Comments"
+            margin="dense"
             variant="outlined"
-          >
+            multiline
+            fullWidth
+          />
+          <Divider />
+        </DialogContent>
+        <DialogActions className={classes.dialogFooter}>
+          <Button onClick={handleClose} color="primary" variant="outlined">
             Cancel
           </Button>
           <Button
             onClick={handleClose}
             color="primary"
             variant="contained"
-            className="ml-2"
+            className={classes.footerBtns}
           >
-            <SendIcon />Submit
+            <SendIcon />
+            Submit
           </Button>
         </DialogActions>
       </Dialog>
