c397f5971bc8094964ee85dc851ba3cdff517d54
stevensam0109 <steven.sam.0109@outlook.com>
2021-02-24 15:24:27 -0500

    Remove test text

--- src/Router/Index.js
+++ src/Router/Index.js
@@ -14,7 +14,7 @@ function Index() {
   return (
     <div style={styles}>
       <Typography variant="h5" gutterBottom>
-        Index TEST 3
+        Index
       </Typography>
       <Typography variant="h6" gutterBottom>
         Active
