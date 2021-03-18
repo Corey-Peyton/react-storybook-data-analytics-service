741260b1de010ae738a81b0de21e2b7ae4a9abb2
stevensam0109 <steven.sam.0109@outlook.com>
2021-03-18 16:23:22 -0400

    Fixed busted index link

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -56,7 +56,7 @@ export default function AppRouter() {
           <Route
             path="/vetting-app/request-analyst-assigned"
             render={() => (
-              <VettingRequestAnalyst userName="Tony Stark" lead="" support={['']}/>
+              <VettingRequestAnalyst userName="Tony Stark" lead="Tony Stark" support={['']}/>
             )}
           />
           <Route
