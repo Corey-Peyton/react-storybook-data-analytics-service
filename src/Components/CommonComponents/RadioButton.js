b63cd074b6b4ba069e718199381fcb05062e252e
stevensam0109 <steven.sam.0109@outlook.com>
2021-04-13 09:11:40 -0400

    More changes

--- src/Components/CommonComponents/RadioButton.js
+++ src/Components/CommonComponents/RadioButton.js
@@ -15,6 +15,7 @@ export function RadioButtons(props) {
     <MUIRadioButtons
       classes={{
         root: classes.Root,
+        size: 'small',
       }}
       {...props}
     ></MUIRadioButtons>
@@ -24,6 +25,7 @@ export function RadioButtons(props) {
 const COLOR = {
   PRIMARY: 'primary',
   SECONDARY: 'secondary',
+  DEFAULT: 'default',
 };
 
 const VARIANT = {
@@ -38,112 +40,44 @@ const SIZE = {
 };
 RadioButtons.propTypes = {
   /**
-    This prop helps users to fill forms faster, especially on mobile devices. The name can be confusing, as it's more like an autofill.
+    If true, the component is checked.
   */
-  autoComplete: PropTypes.string,
+  checked: PropTypes.bool,
   /**
-   If true, the input element will be focused during the first mount.
+  The icon to display when the component is checked.
   */
-  autoFocus: PropTypes.bool,
+  checkedIcon: PropTypes.node,
   /**
     The color of the component
   */
   color: PropTypes.oneOf(Object.values(COLOR)),
   /**
-    The default value of the input element.
+    The default value for a Radio Group. Use when the component is not controlled.
   */
-  defaultValue: PropTypes.any,
+  defaultValue: PropTypes.array,
   /**
-    If true, the textfield will be disabled.
+    If true, the component is disabled.
   */
   disabled: PropTypes.bool,
   /**
-    If true, the input will not have an underline, applies to filled textfields only.
+    The icon to display when the component is unchecked.
   */
-  disableUnderline: PropTypes.bool,
+  icon: PropTypes.node,
   /**
-  End InputAdornment for this component, applies to filled and outlined textfields only.
-  */
-  endAdornment: PropTypes.node,
-  /**
-  If true, the label will be displayed in an error state.
-  */
-  error: PropTypes.bool,
-  /**
-    If true, the textfield will take up the full width of its container.
-  */
-  fullWidth: PropTypes.bool,
-  /**
-  The helper text content.
-   */
-  helpertext: PropTypes.node,
-  /**
-   Textfield id used for a11y
-  */
-  id: PropTypes.string,
-  /**
-  The label content.
-  */
-  label: PropTypes.node,
-  /**
-  If dense or normal, will adjust vertical spacing of this and contained components.
-  */
-  margin: PropTypes.string,
-  /**
-  If true, a textarea element will be rendered instead of an input.
-  */
-  multiline: PropTypes.bool,
-  /**
-  The short hint displayed in the input before the user enters a value.
-  */
-  placeholder: PropTypes.string,
-  /**
-It prevents the user from changing the value of the field (not from interacting with the field), applies to filled textfields only.
-  */
-  readOnly: PropTypes.bool,
-  /**
- If true, the label is displayed as required and the input element` will be required.
+ If true, the input element is required
   */
   required: PropTypes.bool,
   /**
- Number of rows to display when multiline option is set to true.
-  */
-  rows: PropTypes.string,
-  /**
- Maximum number of rows to display when multiline option is set to true.
-  */
-  rowsmax: PropTypes.string,
-  /**
-    Start InputAdornment for this component, applies to filled  and outlined textfields only.
-  */
-  startAdornment: PropTypes.node,
-  /**
-    Render a Select element while passing the Input element to Select as input parameter. If this option is set you must pass the options of the select as children.
-  */
-  select: PropTypes.bool,
-  /**
-    The size of the textfield
-  */
-  size: PropTypes.oneOf(Object.values(SIZE)),
-  /**
-    Type of the input element. It should be a valid HTML5 input type.
-  */
-  type: PropTypes.string,
-  /**
-The value of the input element, required for a controlled component.
+The value of the component. The DOM API casts this to a string.
   */
   value: PropTypes.any,
-  /**
-The variant to use.
-  */
-  variant: PropTypes.any,
 };
 
 RadioButtons.defaultProps = {
-  color: COLOR.PRIMARY,
+  checked: false,
   disabled: false,
-  fullWidth: false,
-  select: false,
-  autoFocus: false,
+  color: COLOR.DEFAULT,
+  size: SIZE.MEDIUM,
+  required: false,
   variant: VARIANT.STANDARD,
 };
--- src/stories/RadioButton.stories.js
+++ src/stories/RadioButton.stories.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {RadioButtons} from '../Components/CommonComponents/RadioButton';
 import {Radio} from '@material-ui/core';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 export default {
   title: 'Molecules/RadioButtons',
@@ -10,11 +14,30 @@ export default {
 export const AllRadioButtons = (args) => {
   return (
     <>
-      <Radio value="a" name="radio-buttons" />
-      <Radio
-        value="b"
-        name="radio-buttons"
-      />
+      <FormControl component="fieldset" className="m-1">
+        <FormLabel component="legend">Vertical radiogroup</FormLabel>
+        <RadioGroup
+          aria-label="numbers"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel value="1" control={<Radio />} label="1" checked/>
+          <FormControlLabel value="2" control={<Radio />} label="2" />
+          <FormControlLabel value="3" control={<Radio />} label="3" />
+        </RadioGroup>
+      </FormControl>
+      <FormControl component="fieldset" className="m-1">
+        <FormLabel component="legend">Horizontal radiogroup</FormLabel>
+        <RadioGroup
+          aria-label="gender"
+          defaultValue="female"
+          name="radio-buttons-group"
+          row
+        >
+          <FormControlLabel value="1" control={<Radio />} label="1" />
+          <FormControlLabel value="2" control={<Radio />} label="2" checked/>
+          <FormControlLabel value="3" control={<Radio />} label="3" />
+        </RadioGroup>
+      </FormControl>
     </>
   );
 };
