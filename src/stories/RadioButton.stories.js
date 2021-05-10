d68629ad38533b26ef0eb116e574d8584003f5ad
stevensam0109 <steven.sam.0109@outlook.com>
2021-05-10 13:22:36 -0400

    Add checkbox legend

--- src/stories/RadioButton.stories.js
+++ src/stories/RadioButton.stories.js
@@ -5,6 +5,7 @@ import {
   FormControl,
   FormLabel,
   FormControlLabel,
+  FormHelperText,
 } from '@material-ui/core';
 
 export default {
@@ -30,6 +31,57 @@ export const Default = (args) => {
     <>
       <FormControl component="fieldset">
         <FormLabel component="legend">Gender</FormLabel>
+        <RadioGroup
+          aria-label="gender"
+          name="gender"
+          value={state.gender}
+          onChange={handleRadioChange}
+        >
+          <FormControlLabel
+            value="female"
+            control={<Radio color="primary" />}
+            label="Female"
+          />
+          <FormControlLabel
+            value="male"
+            control={<Radio color="primary" />}
+            label="Male"
+          />
+          <FormControlLabel
+            value="other"
+            control={<Radio color="primary" />}
+            label="Other"
+          />
+          <FormControlLabel
+            value="disabled"
+            disabled
+            control={<Radio />}
+            label="(Disabled option)"
+          />
+        </RadioGroup>
+      </FormControl>
+    </>
+  );
+};
+
+export const HelperText = (args) => {
+  const [state, setState] = React.useState({
+    gender: null,
+  });
+
+  const handleRadioChange = (event) => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
+
+  return (
+    <>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Gender</FormLabel>
+        <FormHelperText>Helper text</FormHelperText>
         <RadioGroup
           aria-label="gender"
           name="gender"
