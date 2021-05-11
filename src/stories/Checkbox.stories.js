ed59e4a8cf6a71330d066e237dd1aaebf5827333
stevensam0109 <steven.sam.0109@outlook.com>
2021-05-11 10:10:15 -0400

    Add error stories for checkox

--- src/stories/Checkbox.stories.js
+++ src/stories/Checkbox.stories.js
@@ -140,3 +140,133 @@ export const HelperText = (args) => {
     </>
   );
 };
+
+export const Error = (args) => {
+  const [state, setState] = React.useState({
+    gilad: true,
+    jason: false,
+    antoine: false,
+  });
+
+  const handleChbxChange = (event) => {
+    setState({...state, [event.target.name]: event.target.checked});
+  };
+
+  return (
+    <>
+      <FormControl component="fieldset" error={true}>
+        <FormLabel component="legend">Assign responsibility</FormLabel>
+        <FormHelperText>Error text</FormHelperText>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={state.gilad}
+                onChange={handleChbxChange}
+                name="gilad"
+              />
+            }
+            label="Gilad Gray"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={state.jason}
+                onChange={handleChbxChange}
+                name="jason"
+              />
+            }
+            label="Jason Killian"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={state.antoine}
+                onChange={handleChbxChange}
+                name="antoine"
+              />
+            }
+            label="Antoine Llorca"
+          />
+        </FormGroup>
+      </FormControl>
+    </>
+  );
+};
+
+export const Individual = (args) => {
+  const [state, setState] = React.useState({
+    terms: false,
+  });
+
+  const handleChbxChange = (event) => {
+    setState({...state, [event.target.name]: event.target.checked});
+  };
+  return (
+    <>
+      <FormControl component="fieldset">
+        <FormLabel component="legend" className="screen-reader-text">
+          Select if you agree
+        </FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={state.terms}
+                onChange={handleChbxChange}
+                name="terms"
+                color="primary"
+                required
+              />
+            }
+            label="I agree to the Terms and conditions and Privacy policy"
+          />
+        </FormGroup>
+      </FormControl>
+    </>
+  );
+};
+
+export const IndividualError = (args) => {
+  const [state, setState] = React.useState({
+    terms: false,
+  });
+
+  const handleChbxChange = (event) => {
+    setState({...state, [event.target.name]: event.target.checked});
+  };
+  return (
+    <>
+      <FormControl component="fieldset" error={true}>
+        <FormLabel component="legend" className="screen-reader-text">
+          Select if you agree
+        </FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={state.terms}
+                onChange={handleChbxChange}
+                name="terms"
+                color="primary"
+                required
+                error={true}
+              />
+            }
+            label={
+              <>
+                <Typography variant="body2">
+                  I agree to the Terms and conditions and Privacy policy
+                </Typography>
+                <FormHelperText>Error text</FormHelperText>
+              </>
+            }
+          />
+        </FormGroup>
+      </FormControl>
+    </>
+  );
+};
