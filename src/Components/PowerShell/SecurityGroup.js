0f45bb2a8d100aacf184032fc7afb892930ea77d
stevensam0109 <steven.sam.0109@outlook.com>
2021-05-14 13:17:10 -0400

    Virtualized the drop down list

--- src/Components/PowerShell/SecurityGroup.js
+++ src/Components/PowerShell/SecurityGroup.js
@@ -1,34 +1,149 @@
 import React from 'react';
-import {suggestions} from '../../Data/fakeData';
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import {useTranslation} from 'react-i18next';
-import {TextField} from '@material-ui/core';
-import Box from '@material-ui/core/Box';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import {useTheme, makeStyles} from '@material-ui/core/styles';
+import {VariableSizeList} from 'react-window';
+import {Typography} from '@material-ui/core';
 
-function SecurityGroup(props) {
-  const {t} = useTranslation();
-  return (
-    <React.Fragment>
-      <div className="pt-3 pb-3">
-        <Box>
-          <Autocomplete
-            id="securitygroupname"
-            options={suggestions.map((option) => option.subject)}
-            renderInput={(params) => (
-              <TextField
-                required
-                {...params}
-                label={t('Security group name')}
-                variant="outlined"
-                fullWidth
-                className="input-margin"
-              />
-            )}
-          />
-        </Box>
-      </div>
-    </React.Fragment>
-  );
+const LISTBOX_PADDING = 8; // px
+
+function renderRow(props) {
+  const {data, index, style} = props;
+  return React.cloneElement(data[index], {
+    style: {
+      ...style,
+      top: style.top + LISTBOX_PADDING,
+    },
+  });
 }
 
-export default SecurityGroup;
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data) {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+// Adapter for react-window
+const ListboxComponent = React.forwardRef(function ListboxComponent(
+    props,
+    ref,
+) {
+  const {children, ...other} = props;
+  const itemData = React.Children.toArray(children);
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up('sm'), {noSsr: true});
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 36 : 48;
+
+  const getChildSize = (child) => {
+    if (React.isValidElement(child) && child.type === ListSubheader) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
+ListboxComponent.propTypes = {
+  children: PropTypes.node,
+};
+
+function random(length) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+}
+
+const useStyles = makeStyles({
+  listbox: {
+    'boxSizing': 'border-box',
+    '& ul': {
+      padding: 0,
+      margin: 0,
+    },
+  },
+});
+
+const OPTIONS = Array.from(new Array(100000))
+    .map(() => random(10 + Math.ceil(Math.random() * 20)))
+    .sort((a, b) => a.toUpperCase().localeCompare(b.toUpperCase()));
+
+const renderGroup = (params) => [
+  <ListSubheader key={params.key} component="div">
+    {params.group}
+  </ListSubheader>,
+  params.children,
+];
+
+export default function Virtualize() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className="pb-3  pt-3">
+        <Autocomplete
+          id="securitygrouup"
+          disableListWrap
+          classes={classes}
+          ListboxComponent={ListboxComponent}
+          renderGroup={renderGroup}
+          options={OPTIONS}
+          fullWidth
+          groupBy={(option) => option[0].toUpperCase()}
+          renderInput={(params) => (
+            <TextField {...params} variant="outlined" label="Security group" />
+          )}
+          renderOption={(option) => <Typography noWrap>{option}</Typography>}
+        />
+      </div>
+    </>
+  );
+}
