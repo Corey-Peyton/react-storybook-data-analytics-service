88e45525770ff1db6aa126e00eef4351a7d36daa
stevensam0109 <steven.sam.0109@outlook.com>
2021-07-19 18:27:13 -0400

    snackbar accessible width

--- src/Components/CommonComponents/Snackbar.js
+++ src/Components/CommonComponents/Snackbar.js
@@ -3,12 +3,25 @@ import PropTypes from 'prop-types';
 // import {useTranslation} from 'react-i18next';
 import {Snackbar as MUISnackbar} from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.down('xs')]: {
+      'bottom': theme.spacing(10),
+      '& .MuiAlert-root': {
+        width: '100%',
+      },
+    },
+  },
+}));
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 export function Snackbar(props) {
+  const classes = useStyles();
   const handleClick = (e) => {
     e.stopPropagation();
   };
@@ -23,6 +36,7 @@ export function Snackbar(props) {
         horizontal: 'left',
       }}
       onClick={handleClick}
+      className={classes.root}
     >
       <Alert onClose={props.handleClose} severity={props.severity}>
         {props.message}
